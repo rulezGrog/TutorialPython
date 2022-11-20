57df4a7f6e2775073b771b1f8cba92ae3bb608bb
DESKTOP-GIMLI\rulez <rulezgrog@gmail.com>
2022-11-20 21:03:35 +0100

    # Primer día del curso de Python de MoureDev

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario: 
+# Nuestro Hola mundo en Python
+print("Hola Python")
+print('Hola Python5')
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+'''
+Esto tambien es
+un comenatrio 
+de varias líneas
+'''
+
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(True)) #Tipo 'bool'
+print(type(1 + 3j)) # Tipo complejo
+
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType' (porque está preguntado por el tipo de print(), que es una función)
---
+++ README.md
@@ -0,0 +1,1 @@
+## Repositorio del tutorial de Python de MoureDev
